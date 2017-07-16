620a70c11ae432ed86a72c54303b49d77fb12c62
pinoChang <z8817176@163.com>
2017-07-16 13:51:36 +0800

    '基本布局未完成'

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <elmeHeader></elmeHeader>
+    <section class="leader">
+      <span>商品</span>
+      <span>评价</span>
+      <span>什么</span>
+    </section>
+    <section class="main">
+      我才是主体
+    </section>
+    <footer>
+      我是最下面
+    </footer>
+  </div>
+</template>
+
+<script>
+import elmeHeader from '@/components/header/header'
+export default {
+  name: 'app',
+  components:{elmeHeader}
+}
+</script>
+
+<style lang="scss">
+#app {
+  text-align: center;
+  .leader{
+    display: flex;
+    span{
+      flex:1;
+    }
+  }
+}
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="elme-header">
+    <h1>我是头部</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
