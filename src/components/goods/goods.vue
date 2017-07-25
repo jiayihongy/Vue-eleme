1d8ade151a0e2b7923dca0e155680e6df1e497bf
haoz <z8817176@163.com>
2017-07-26 00:06:44 +0800

    商品页图片为什么无法获取

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -5,8 +5,27 @@
         <li>{{good.name}}</li>
       </ul>
     </div>
-    <div class="goods-details">
-
+    <div class="goods-list">
+      <div class="goods-details" v-for="good in goods">
+        <div>
+          <p>{{good.name}}</p>
+        </div>
+        <ul v-for="food in good.foods">
+          <li>
+            <div class="goods-img">
+              <img :scr="food.icon" style="width:64px;height:64px">
+            </div>
+            <div class="goods-descripthon">
+              <p>{{food.name}}</p>
+              <p>{{food.description}}</p>
+              <p>
+                <span>月售{{food.sellCount}}份</span>
+                <span>{{food.rating}}%好评率</span>
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -17,14 +36,14 @@ export default {
   name: 'goods',
   data () {
     return {
-      goods:[]
+      goods:[],
     }
   },
   created(){
     axios.get('/api/goods')
     .then(res=>{
+      // 获取商品对象数组
       this.goods=res.data.data;
-      console.log(this.goods)
     })
     .catch(err => {
       alert('获取商品数据失败');
@@ -35,5 +54,19 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+@import "../../commen/css/all.scss";
+  .goods{
+    display: flex;
+    .hot-sell{
+      box-sizing: border-box;
+      ul{
+        li{
+          width: 80px;
+        }
+      }
+    }
+    .goods-list{
 
+    }
+  }
 </style>
